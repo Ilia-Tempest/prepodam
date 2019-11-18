de708e484c1d5e9144b7172b1d73595b60eaec3c
Ilia-Tempest <51461710+Ilia-Tempest@users.noreply.github.com>
2019-11-18 05:39:50 +0500

    Сверстал страницы студенческой программы и начал верстать страницы дополнительного образования

---
+++ src/js/date-student.js
@@ -0,0 +1,10 @@
+$(function() {
+	
+  $.datepicker.setDefaults($.datepicker.regional['ru']);
+  $('#datep').datepicker({
+    showOn: "both",
+    buttonImage: "../../img/student-program/Group.svg",
+    buttonImageOnly: true
+});
+    
+});
